55b6b5db6ab84165f4ca9fd6024583098e195bbf
Francisco Ozuna <ozuna252@hotmail.com>
2020-10-07 18:24:17 -0400

    Implemented code to build AlertDialog which launches when the user click on the UPLOAD IMAGE button

--- app/src/main/java/com/example/automaticappareloutliner/MainActivity.java
+++ app/src/main/java/com/example/automaticappareloutliner/MainActivity.java
@@ -1,7 +1,9 @@
 package com.example.automaticappareloutliner;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -22,8 +24,27 @@ public class MainActivity extends AppCompatActivity {
         uploadButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                // selectImage();
+                selectImage();
             }
         });
+    }
+
+    private void selectImage() {
+        final CharSequence[] options = {"Take Photo", "Choose from Gallery", "Cancel"};
+        AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(this);
+        alertDialogBuilder.setTitle("Add photo!");
+        alertDialogBuilder.setItems(options, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                if (options[which].equals("Take Photo")) {
+                    //TODO: Use an intent to capture a photo using the camera.
+                } else if (options[which].equals("Choose from Gallery")) {
+                    //TODO: Use an intent to allow the user to upload a photo from the local device gallery.
+                } else if (options[which].equals("Cancel")) {
+                    dialog.dismiss();
+                }
+            }
+        });
+        alertDialogBuilder.show();
     }
 }
