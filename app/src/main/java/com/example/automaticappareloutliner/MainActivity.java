8812c64f38937d1a1541aefb91106db8ed27b829
Francisco Ozuna <ozuna252@hotmail.com>
2020-10-07 18:17:36 -0400

    Initialized basic ui elements (text view, image view, button) to be used in the app.

--- app/src/main/java/com/example/automaticappareloutliner/MainActivity.java
+++ app/src/main/java/com/example/automaticappareloutliner/MainActivity.java
@@ -3,12 +3,27 @@ package com.example.automaticappareloutliner;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
+    ImageView image;
+    Button uploadButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        image = (ImageView) findViewById(R.id.imageView_fashionItem);
+        uploadButton = (Button) findViewById(R.id.button_upload);
+
+        uploadButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // selectImage();
+            }
+        });
     }
 }
