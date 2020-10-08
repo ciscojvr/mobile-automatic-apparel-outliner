b0482d88f4760c22e51e4591e5b6d893921c45ab
Francisco Ozuna <ozuna252@hotmail.com>
2020-10-07 23:13:23 -0400

    Implemented intent to be used when user uploads picture from internal storage.

--- app/src/main/java/com/example/automaticappareloutliner/MainActivity.java
+++ app/src/main/java/com/example/automaticappareloutliner/MainActivity.java
@@ -7,16 +7,26 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.ActivityNotFoundException;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.database.Cursor;
 import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.Bundle;
 import android.provider.MediaStore;
+import android.util.Base64;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.Toast;
 
+import java.io.ByteArrayOutputStream;
+
 public class MainActivity extends AppCompatActivity {
     static final int REQUEST_IMAGE_CAPTURE = 1;
+    static final int PROVIDE_IMAGE_FROM_GALLERY = 2;
+
+    private String Document_img1="";
 
     ImageView fashionImage;
     ImageView segmentedImage;
@@ -53,7 +63,7 @@ public class MainActivity extends AppCompatActivity {
                 if (options[which].equals("Take Photo")) {
                     dispatchTakePictureIntent();
                 } else if (options[which].equals("Choose from Gallery")) {
-                    //TODO: Use an intent to allow the user to upload a photo from the local device gallery.
+                    dispatchChoosePictureIntent();
                 } else if (options[which].equals("Cancel")) {
                     dialog.dismiss();
                 }
@@ -71,6 +81,15 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
+    private void dispatchChoosePictureIntent() {
+        Intent choosePictureIntent = new   Intent(Intent.ACTION_PICK,android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+        try {
+            startActivityForResult(choosePictureIntent, PROVIDE_IMAGE_FROM_GALLERY);
+        } catch (ActivityNotFoundException e) {
+            Toast.makeText(getApplicationContext(), "Activity Not Found Exception.", Toast.LENGTH_SHORT).show();
+        }
+    }
+
     @Override
     protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
@@ -80,6 +99,49 @@ public class MainActivity extends AppCompatActivity {
             fashionImage.setImageBitmap(imageBitmap);
             segmentedImage.setImageBitmap(imageBitmap);
             outlinedImage.setImageBitmap(imageBitmap);
+        } else if (requestCode == PROVIDE_IMAGE_FROM_GALLERY && resultCode == RESULT_OK) {
+            Uri selectedImage = data.getData();
+            fashionImage.setImageURI(selectedImage);
+            segmentedImage.setImageURI(selectedImage);
+            outlinedImage.setImageURI(selectedImage);
+
+//            String[] filePath = { MediaStore.Images.Media.DATA };
+//            Cursor c = getContentResolver().query(selectedImage,filePath, null, null, null);
+//            c.moveToFirst();
+//            int columnIndex = c.getColumnIndex(filePath[0]);
+//            String picturePath = c.getString(columnIndex);
+//            c.close();
+//            Log.w("path of image from gallery......******************.........", picturePath+"");
+//            Bitmap thumbnail = (BitmapFactory.decodeFile(picturePath));
+//            thumbnail=getResizedBitmap(thumbnail, 400);
+//            Log.w("path of image from gallery......******************.........", picturePath+"");
+//            fashionImage.setImageBitmap(thumbnail);
+//            segmentedImage.setImageBitmap(thumbnail);
+//            outlinedImage.setImageBitmap(thumbnail);
+//            BitMapToString(thumbnail);
         }
     }
+
+//    public String BitMapToString(Bitmap userImage1) {
+//        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+//        userImage1.compress(Bitmap.CompressFormat.PNG, 60, baos);
+//        byte[] b = baos.toByteArray();
+//        Document_img1 = Base64.encodeToString(b, Base64.DEFAULT);
+//        return Document_img1;
+//    }
+//
+//    public Bitmap getResizedBitmap(Bitmap image, int maxSize) {
+//        int width = image.getWidth();
+//        int height = image.getHeight();
+//
+//        float bitmapRatio = (float)width / (float) height;
+//        if (bitmapRatio > 1) {
+//            width = maxSize;
+//            height = (int) (width / bitmapRatio);
+//        } else {
+//            height = maxSize;
+//            width = (int) (height * bitmapRatio);
+//        }
+//        return Bitmap.createScaledBitmap(image, width, height, true);
+//    }
 }
